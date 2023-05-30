f1ed276659f8ce0ffcd5c1769e06db4db01f3798
Sean Jung <seanjung1129@gmail.com>
2023-05-30 11:39:27 +0900

    calender data 불러오기

--- app/src/main/java/com/example/login/CalendarActivity.java
+++ app/src/main/java/com/example/login/CalendarActivity.java
@@ -12,12 +12,21 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class CalendarActivity extends AppCompatActivity {
 
     private String theDay = "";
+    private String selectedDay;
+    private int clickCount = 0;
+    private DatabaseReference mDatabase;
     Button addBtn;
     Button goBackBtn;
 
@@ -32,25 +41,61 @@ public class CalendarActivity extends AppCompatActivity {
         int year = today.get(Calendar.YEAR);
         int month = today.get(Calendar.MONTH);
         int day = today.get(Calendar.DATE);
-        
-        // Calendar 들어오면 바로 날짜 선택 켜지고 "확인"누르면 바로 일별 매출 기록 page가 켜짐
-//        DatePickerDialog dlg = new DatePickerDialog(CalendarActivity.this, new DatePickerDialog.OnDateSetListener() {
-//            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-//                theDay = year + " " + month + " " + dayOfMonth;
-//                Log.d("day: ", theDay);
-//
-//            }
-//        }, year, month, day);
-//        dlg.show();
+
 
         calendarView = findViewById(R.id.calendarView);
+
+
+
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
-                Intent intent = new Intent(CalendarActivity.this, DailyActivity.class);
+                clickCount++;
                 theDay = month+1 + "월 " + day + "일 매출 입력";
-                intent.putExtra("date", theDay);
 
-                startActivity(intent);
+                if (clickCount == 1) {
+                    selectedDay = theDay;
+                    //날에 해당하는 매출 나옴
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("매출").child(theDay);
+                    myRef.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            // 데이터가 있는 경우
+                            if (dataSnapshot.exists()) {
+                                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                    String strMonth = snapshot.child("월").getValue().toString();
+                                    String strDay = snapshot.child("일").getValue().toString();
+                                    if (strMonth.equals(month) && strDay.equals(day)) {
+                                        // Get data from snapshot
+                                        Long laundry = snapshot.child("세탁/건조").getValue(Long.class);
+                                        Long shoes = snapshot.child("운동화").getValue(Long.class);
+                                        Long detergent = snapshot.child("세탁용품").getValue(Long.class);
+
+                                        // TODO: Update your UI using the data
+                                        TextView showEarn = findViewById(R.id.showEarn);
+                                        showEarn.setText("세탁/건조: " + laundry + ", 운동화: " + shoes + ", 세탁용품: " + detergent);
+                                    }
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                            // Failed to read value
+                            Log.w("Firebase", "Failed to read value.", error.toException());
+                        }
+                    });
+                }
+
+                    if (clickCount == 2 && selectedDay.equals(theDay)) {
+                    Intent intent = new Intent(CalendarActivity.this, DailyActivity.class);
+                    intent.putExtra("date", theDay);
+                    startActivity(intent);
+                    clickCount = 0;
+                } else {
+                    clickCount = 0;
+                }
             }
         });
 
